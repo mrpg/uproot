e77d1fa2d0cf9dae69d989e8ab217a1f206905d8
Holger Gerhardt <holger.gerhardt@uni-bonn.de>
2025-09-01 14:03:43 +0200

    Fix typo in upgrade.sh

--- src/uproot/static/foreign/upgrade.sh
+++ src/uproot/static/foreign/upgrade.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-# Upgrade AlpinJS and Bootstrap CSS + JS
+# Upgrade Alpine.js and Bootstrap CSS + JS
 rm -f alpine.min.js  bootstrap.bundle.min.js  bootstrap.bundle.min.js.map  bootstrap.min.css  bootstrap.min.css.map
 wget -q "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" \
      "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/js/bootstrap.bundle.min."{js,js.map} \
