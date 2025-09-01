a5b4e1174164fbb4d8d31dc7411e00c35c23dad9
Holger Gerhardt <holger.gerhardt@uni-bonn.de>
2025-09-01 13:56:44 +0200

    Include Bootstrap Icons font only in admin.css

--- src/uproot/static/foreign/upgrade.sh
+++ src/uproot/static/foreign/upgrade.sh
@@ -2,12 +2,21 @@
 
 set -euo pipefail
 
+# Upgrade AlpinJS and Bootstrap CSS + JS
 rm -f alpine.min.js  bootstrap.bundle.min.js  bootstrap.bundle.min.js.map  bootstrap.min.css  bootstrap.min.css.map
-
 wget -q "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" \
      "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/js/"{bootstrap.bundle.min.js,bootstrap.bundle.min.js.map} \
      "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/css/"{bootstrap.min.css,bootstrap.min.css.map}
-
 mv cdn.min.js alpine.min.js
+
+# Upgrade Bootstrap Icons CSS
+cd ../fonts/bootstrap-icons-1.x.x
+rm -f bootstrap-icons.min.css
+wget -q "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.x.x/font/bootstrap-icons.min.css"
+
+# Upgrade Bootstrap Icons font files
+cd fonts
+rm -f bootstrap-icons.woff  bootstrap-icons.woff2
+wget -q "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.x.x/font/fonts/bootstrap-icons."{woff,woff2}
 
 echo OK
