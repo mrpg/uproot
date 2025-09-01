a67b1cdbbbc725bb97871a7a4723cfbc441e9e16
Holger Gerhardt <holger.gerhardt@uni-bonn.de>
2025-09-01 14:01:43 +0200

    Tidy up upgrade.sh

--- src/uproot/static/foreign/upgrade.sh
+++ src/uproot/static/foreign/upgrade.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 # Upgrade AlpinJS and Bootstrap CSS + JS
 rm -f alpine.min.js  bootstrap.bundle.min.js  bootstrap.bundle.min.js.map  bootstrap.min.css  bootstrap.min.css.map
 wget -q "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" \
-     "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/js/"{bootstrap.bundle.min.js,bootstrap.bundle.min.js.map} \
-     "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/css/"{bootstrap.min.css,bootstrap.min.css.map}
+     "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/js/bootstrap.bundle.min."{js,js.map} \
+     "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/css/bootstrap.min."{css,css.map}
 mv cdn.min.js alpine.min.js
 
 # Upgrade Bootstrap Icons CSS
@@ -19,4 +19,5 @@ cd fonts
 rm -f bootstrap-icons.woff  bootstrap-icons.woff2
 wget -q "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.x.x/font/fonts/bootstrap-icons."{woff,woff2}
 
+# Finish
 echo OK
