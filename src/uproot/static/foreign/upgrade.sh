771392a2ee8cb1abc49563a861849310e99ffa93
Max R. P. Grossmann <m@max.pm>
2025-09-02 10:01:07 +1000

    Streamline icons embedding

--- src/uproot/static/foreign/upgrade.sh
+++ src/uproot/static/foreign/upgrade.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 # Upgrade Alpine.js and Bootstrap CSS + JS
-rm -f alpine.min.js  bootstrap.bundle.min.js  bootstrap.bundle.min.js.map  bootstrap.min.css  bootstrap.min.css.map
-wget -q "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" \
-     "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/js/bootstrap.bundle.min."{js,js.map} \
-     "https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/css/bootstrap.min."{css,css.map}
+rm -f alpine.min.js bootstrap.bundle.min.js* bootstrap.min.css*
+curl -sS -O "https://cdn.jsdelivr.net/npm/alpinejs@3/dist/cdn.min.js"
+curl -sS -O "https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js"
+curl -sS -O "https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js.map"
+curl -sS -O "https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css"
+curl -sS -O "https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css.map"
 mv cdn.min.js alpine.min.js
 
 # Upgrade Bootstrap Icons CSS
-cd ../fonts/bootstrap-icons-1.x.x
 rm -f bootstrap-icons.min.css
-wget -q "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.x.x/font/bootstrap-icons.min.css"
+curl -sS -O "https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/bootstrap-icons.min.css"
 
 # Upgrade Bootstrap Icons font files
+rm -rf fonts
+mkdir fonts
 cd fonts
-rm -f bootstrap-icons.woff  bootstrap-icons.woff2
-wget -q "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.x.x/font/fonts/bootstrap-icons."{woff,woff2}
 
-# Finish
-echo OK
+curl -sS -O "https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/fonts/bootstrap-icons.woff"
+curl -sS -O "https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/fonts/bootstrap-icons.woff2"
