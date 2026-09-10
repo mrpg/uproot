3f51875f32d0c9297da8d86d15152c5bc37426b5
Max R. P. Grossmann <m@max.pm>
2026-09-10 08:27:27 +0000

    Increase MAX_MULTIVIEW_PLAYERS to 49

--- src/uproot/_static/monitor.js
+++ src/uproot/_static/monitor.js
@@ -9,7 +9,7 @@
 
 const EXTRA_FIELDS = ["started", "label", "round", "_uproot_group", "member_id", "_uproot_dropout"];
 const MONITOR_PRIORITY_FIELDS = ["id", "label", "player", "page", "progress", "lastSeen", "dropout", "round", "group", "member_id"];
-const MAX_MULTIVIEW_PLAYERS = 48;
+const MAX_MULTIVIEW_PLAYERS = 49;
 const HEARTBEAT_DURATION_MS = 5000;
 const UPDATE_DEBOUNCE_MS = 150;
 
